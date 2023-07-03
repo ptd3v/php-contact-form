b2225b23174d728de24a3ca5ed8e2ad6fae372a7
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-07-03 18:21:12 +0100

    PHP Completed

--- process.php
+++ process.php
@@ -1,10 +1,17 @@
 <?php
-//Get form data
+//Check if the request method is POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //Get the form data from the HTML file
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-//Validate
-
-//Process
-
-//Display a success message
-
+    //Basic data validation
+    if (empty($name) || empty($email) || empty($message)) {
+        //Stop the process if any fields are missing, display an error message
+        die('Please fill in all fields');
+    }
+    //Display a success message
+    echo 'All sent! End of Demo';
+}
 ?>
